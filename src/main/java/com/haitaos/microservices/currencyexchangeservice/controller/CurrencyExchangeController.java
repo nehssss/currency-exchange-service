63b76ae32a01de9582acddd263e9f08c4240a8a5
nehssss <562410105@qq.com>
2023-05-04 00:05:01 -0500

    use k8s to deploy

--- src/main/java/com/haitaos/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/haitaos/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.haitaos.microservices.currencyexchangeservice.controller;
 
 import com.haitaos.microservices.currencyexchangeservice.entity.CurrencyExchange;
 import com.haitaos.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,9 @@ public class CurrencyExchangeController {
 
     private Environment environment;
 
+    private Logger logger =
+            LoggerFactory.getLogger(CircuitBreakerController.class);
+
     public CurrencyExchangeController(Environment environment,
                                       CurrencyExchangeRepository currencyExchangeRepository) {
         this.currencyExchangeRepository = currencyExchangeRepository;
@@ -24,12 +29,17 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue (
             @PathVariable String from,
             @PathVariable String to) {
+        this.logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to Find data for " + from + " to " + to);
         }
         String port = environment.getProperty("local.server.port");
-        currencyExchange.setEnvironment(port);
+
+        String host = environment.getProperty("HOSTNAME");
+        String version = "v2";
+        currencyExchange.setEnvironment(port + " " + host + " " + version);
 
         return currencyExchange;
     }
